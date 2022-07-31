aae17a33646d18048a06d77f370222cbf0212e04
Rubel Hossain <rubrex96@hotmail.com>
2022-07-31 17:22:45 +0600

    shorthands

---
+++ Introduction to JS/shorthands.js
@@ -0,0 +1,18 @@
+// Variable Declaration
+var tomatoPrice = 25;
+// Variable Update
+tomatoPrice = 30;
+
+// Add something to the previous value
+var price1 = 31;
+var price2 = 15;
+
+// Set a new value
+// Price1 = 41
+
+// Add some value to the existing value
+price1 = price1 + 10;
+console.log(price1);
+
+price1 += 5;
+console.log(price1);
