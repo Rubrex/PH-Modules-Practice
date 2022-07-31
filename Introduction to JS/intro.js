0dd2a038fc72d823aa815b3140a83e28fba652ea
Rubel Hossain <rubrex96@hotmail.com>
2022-07-31 16:51:39 +0600

    Simple Mathematical Operations

---
+++ Introduction to JS/intro.js
@@ -0,0 +1,8 @@
+var onionPrice = 20;
+var eggPrice = 10;
+
+var totalPrice = onionPrice + eggPrice;
+
+var priceDifference = onionPrice - eggPrice;
+// console.log(totalPrice);
+console.log(priceDifference);
