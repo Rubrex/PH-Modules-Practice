fb818fcaea47ef4ccdc99b68a9003bb5031fcd02
Rubel Hossain <rubrex96@hotmail.com>
2022-08-01 00:42:14 +0600

    Basic array declaration

---
+++ Array Concepts and Conditionals/array.js
@@ -0,0 +1,10 @@
+// Array Variable
+
+let age = [54, 65, 32, 36, 54, 2, 45, 85];
+let fee = [4000, 5000, 3000, 7000, 6000];
+let names = ["Aryan", "Jhon", "Rifat", "Ayesha"];
+let vowels = ["a", "e", "i", "o", "u"];
+
+console.log(names[2]);
+
+console.log(names[names.indexOf("Rifat")]);
