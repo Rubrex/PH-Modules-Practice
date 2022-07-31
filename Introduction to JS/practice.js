c05f2232d456347423b4e9408f5d1caa750dcc08
Rubel Hossain <rubrex96@hotmail.com>
2022-07-31 17:52:12 +0600

    practices

---
+++ Introduction to JS/practice.js
@@ -0,0 +1,9 @@
+// Practice 1
+// Subjects with Marks
+let Mathematics = 75.6,
+  Biology = 65.1,
+  Chemistry = 49.8,
+  Bangla = 86.5;
+let AvgMarks = (Mathematics + Biology + Chemistry + Bangla) / 4;
+
+console.log(Mathematics);
