ccadc71626672b7476ed447b8a63ecc4c9bec38e
Rubel Hossain <rubrex96@hotmail.com>
2022-08-14 14:14:52 +0600

    addEventListener, calling Function, changing Styles from JS

--- Event Handers/event-/app.js
+++ Event Handers/event-/app.js
@@ -1,1 +1,24 @@
-console.log("Hello");
+// Greet when clicked on Button
+function greetings() {
+  const targetText = document.getElementById("greetMeText");
+  targetText.innerText = "Hola Amigos! You Found it";
+}
+// Write Name on Header after CLicking Submit Button
+document.getElementById("basic-addon2").addEventListener("click", function () {
+  const inputText = document.getElementById("nameInput");
+  const targetText = document.getElementById("recipientName");
+
+  //   console.log(typeof inputText.value, inputText.value.length, inputText.value);
+  if (inputText.value !== "") {
+    //Check if input field has a value
+    targetText.innerText = inputText.value;
+    inputText.value = "";
+  }
+});
+// Add Background Color to each Section
+const sections = document.getElementsByTagName("section");
+for (let section of sections) {
+  section.style.backgroundColor = "#E8F9FD";
+  section.style.padding = "50px";
+  section.style.marginBottom = "50px";
+}
