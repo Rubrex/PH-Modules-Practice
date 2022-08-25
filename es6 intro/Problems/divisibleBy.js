4c4f9ae014600eeb1318f6e23800dbf774d16678
Rubel Hossain <rubrex96@hotmail.com>
2022-08-26 00:19:18 +0600

    Conclusions

--- es6 intro/Problems/divisibleBy.js
+++ es6 intro/Problems/divisibleBy.js
@@ -9,6 +9,14 @@ const numbers = [33, 15, 23, 54, 21, 20, 60, 68, 88, 10, 200, 100, 450];
 
 // Solution 1
 const divisible = numbers.filter((num) => num % 10 == 0);
-
+// Compare with find() method
+const findIt = numbers.find((num) => num % 10 == 0);
 // Output
 console.log(divisible);
+console.log(findIt);
+
+// Conclusions
+/* 
+    .filter() method returns all the elements which are divisible by 10 using
+    .find() method returns only the first matched element which is divisible by 10
+*/
