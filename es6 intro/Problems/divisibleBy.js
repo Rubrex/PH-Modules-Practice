ee506d135a88648cc10d86960480c9b49e671baf
Rubel Hossain <rubrex96@hotmail.com>
2022-08-26 00:15:55 +0600

    divisible by 10

---
+++ es6 intro/Problems/divisibleBy.js
@@ -0,0 +1,14 @@
+/* 
+You are given an array say: [33, 50, 79, 78, 90, 101, 30 ]. Now
+return/get all the elements which are divisible by 10 using
+array.filter() method. 
+*/
+
+// Global Variable
+const numbers = [33, 15, 23, 54, 21, 20, 60, 68, 88, 10, 200, 100, 450];
+
+// Solution 1
+const divisible = numbers.filter((num) => num % 10 == 0);
+
+// Output
+console.log(divisible);
