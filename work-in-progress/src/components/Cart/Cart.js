2eb19ceb78edde98100b3065dfd74345a98f3fb8
Rubel Hossain <rubrex96@hotmail.com>
2022-09-27 19:05:14 +0600

    cart calculations added

--- work-in-progress/src/components/Cart/Cart.js
+++ work-in-progress/src/components/Cart/Cart.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./Cart.css";
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+  const { cart } = props;
+
+  // Tax and Total Calculations
+  let total = 0;
+  let shippingCost = 0;
+  for (const item of cart) {
+    total = total + item.price;
+    shippingCost = shippingCost + item.shipping;
+  }
+  let taxCost = parseFloat((total * 0.1).toFixed(2));
+  let grandTotal = (total + shippingCost + taxCost).toFixed(2);
+
   return (
     <div className="cart">
       <h3>Order Summary</h3>
       <div className="cart-info">
         <p>Selected Items: {cart.length}</p>
-        <p>Total Price: </p>
-        <p>Total Shipping Charge: </p>
-        <p>Tax: </p>
-        <h2>Grand Total: </h2>
+        <p>Total Price: {total}</p>
+        <p>Total Shipping Charge: {shippingCost}</p>
+        <p>Tax: {taxCost}</p>
+        <h3>Grand Total: ${grandTotal}</h3>
       </div>
       <div className="btn-group">
         <button className="clear-btn">Clear Cart</button>
