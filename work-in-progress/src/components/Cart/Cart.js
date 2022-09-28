7ebc630f363b9502239c98f12714ef40556b39ca
Rubel Hossain <rubrex96@hotmail.com>
2022-09-28 18:45:33 +0600

    addToCart event handler to cart calculation from localStorage fixed

--- work-in-progress/src/components/Cart/Cart.js
+++ work-in-progress/src/components/Cart/Cart.js
@@ -5,25 +5,27 @@ import React from "react";
 import "./Cart.css";
 const Cart = (props) => {
   const { cart } = props;
-
+  // console.log(cart);
   // Tax and Total Calculations
   let total = 0;
-  let shippingCost = 0;
-  for (const item of cart) {
-    total = total + item.price;
-    shippingCost = shippingCost + item.shipping;
+  let shipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total = total + product.price * quantity;
+    shipping = shipping + product.shipping;
   }
-  let taxCost = parseFloat((total * 0.1).toFixed(2));
-  let grandTotal = (total + shippingCost + taxCost).toFixed(2);
+  let tax = parseFloat((total * 0.1).toFixed(2));
+  let grandTotal = (total + shipping + tax).toFixed(2);
 
   return (
     <div className="cart">
       <h3>Order Summary</h3>
       <div className="cart-info">
-        <p>Selected Items: {cart.length}</p>
+        <p>Selected Items: {quantity}</p>
         <p>Total Price: ${total}</p>
-        <p>Total Shipping Charge: ${shippingCost}</p>
-        <p>Tax: ${taxCost}</p>
+        <p>Total Shipping Charge: ${shipping}</p>
+        <p>Tax: ${tax}</p>
         <h3>Grand Total: ${grandTotal}</h3>
       </div>
       <div className="btn-group">
--- work-in-progress/src/components/Shop/Shop.js
+++ work-in-progress/src/components/Shop/Shop.js
@@ -7,31 +7,45 @@ const Shop = () => {
   // States
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+
   // useEffect
   useEffect(() => {
     fetch("products.json")
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
+
   // Get data from local storage
   useEffect(() => {
     const storedCart = getStoredCart();
-    const newCart = [];
+    const savedCart = [];
     // Looping through id from products and updating quantity from localStorage  to cart object
     for (const id in storedCart) {
       const addedProduct = products.find((product) => product.id === id);
       if (addedProduct) {
-        addedProduct.quantity = storedCart[id];
-        newCart.push(addedProduct);
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
       }
     }
-    setCart(newCart);
+    setCart(savedCart);
   }, [products]);
+
   // event Handlers
-  const addToCardHandler = (product) => {
-    const newCart = [...cart, product];
+  const addToCardHandler = (selectedProduct) => {
+    let newCart = [];
+    const exists = cart.find((product) => product.id === selectedProduct.id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
+
     setCart(newCart);
-    addToDb(product.id);
+    addToDb(selectedProduct.id);
   };
   return (
     <div className="shop-container container">
